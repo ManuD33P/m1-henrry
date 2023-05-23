4c1c0314886d90f5a5e2154fd107e520b0cb602f
ManuD33P <manu07x@hotmail.com>
2023-05-23 01:10:02 -0300

    Ejercicios 01 Resueltos.

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,9 +1,28 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+const numArray = [...num].reverse();
+let result =0;
+numArray.forEach((element,index) => {
+   result += element * Math.pow(2,index);
+});
+return result;
+}
 
-function DecimalABinario(num) {}
-
+function DecimalABinario(num) {
+let cociente=num;
+let resultados = [];
+while(cociente>0){
+   resultados.push(cociente%2);
+   cociente = Math.floor(cociente / 2);
+}
+let nuevoBinario ="";
+resultados
+.reverse()
+.forEach(element=> nuevoBinario+=element);
+return nuevoBinario;
+}
+console.log(DecimalABinario(4))
 module.exports = {
    BinarioADecimal,
    DecimalABinario,
